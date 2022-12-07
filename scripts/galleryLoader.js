80096c07af5f6a9b7d8ea418392f21c6d82c4809
MeekoDev <DiegoCru1024@hotmail.com>
2022-12-06 20:53:17 -0500

    Upload

---
+++ scripts/galleryLoader.js
@@ -0,0 +1,23 @@
+window.onload = function (){
+    for(var i = 0; i < 50; i++){
+        var container = document.getElementById("test")
+        var img = document.createElement("img")
+        img.src = `../img/mainGallery/gallery(${i+1}).jpg`
+        img.setAttribute("onclick","openIMG(this.src);")
+        container.append(img)
+    }
+}
+
+function openIMG(img){
+    var imageBox = document.getElementById("fullImageBox")
+    var imageItem = document.getElementById("fullImage")
+
+    imageBox.style.zIndex = "99999"
+    imageBox.style.display = "flex"
+    imageItem.src = img
+}
+
+function closeIMG(){
+    var imageBox = document.getElementById("fullImageBox")
+    imageBox.style.display = "none"
+}
